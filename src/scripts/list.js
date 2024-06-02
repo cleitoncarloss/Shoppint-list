98b19c3dc026e0f210e26bdb46c567530108ed73
Cleiton carlos <cleitoncarlos.programmer@gmail.com>
2024-06-02 15:53:08 -0300

    separando o arquivo em component

--- src/scripts/list.js
+++ src/scripts/list.js
@@ -1,35 +1,9 @@
 import supabase from './supabase.js'
+import listing from './listing.js'
 
 const { data } = await supabase.from('users').select('*')
 const content = document.querySelector('.content')
 
-let imgEdit
-let imgDelete
-let elementHtml
-let div
-let figure
-
 data.map(element => {
-  div = document.createElement('div')
-  div.classList.add('content__container')
-  figure = document.createElement('figure')
-  figure.classList.add('content__figure')
-
-  imgEdit = document.createElement('img')
-  imgEdit.src = '../images/edit.png'
-  imgEdit.classList.add('content__buttonEdit')
-
-  imgDelete = document.createElement('img')
-  imgDelete.src = '../images/delete.png'
-  imgDelete.classList.add('content__buttonDelete')
-  elementHtml = document.createElement('span')
-
-  elementHtml.textContent = element.name
-  elementHtml.classList.add('content__list')
-
-  div.append(elementHtml)
-  div.append(figure)
-  figure.append(imgEdit, imgDelete)
-
-  content.append(div)
+  content.innerHTML += listing(element.name)
 })
