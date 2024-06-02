2ba2a98e42800574cae909494c27482179e17e31
cleitoncarloss <cleitoncarlos.programmer@gmail.com>
2024-06-02 13:58:24 -0300

    agora as configuracoes do banco estao separadas em um arquivo diferente

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import supabase from "./supabase.js"
 
 const valueList = document.querySelector('.content__value')
-const supabase = createClient(
-  'https://gyezcramcvtfpcwcwbzt.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5ZXpjcmFtY3Z0ZnBjd2N3Ynp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg2NDY5MzUsImV4cCI6MjAyNDIyMjkzNX0.789_Mu3PGfJ5kIXl2x_1Bp76M6Cs54mmch-n1FQZrFw'
-)
-
 async function insert() {
   const { data } = await supabase
     .from('users')
