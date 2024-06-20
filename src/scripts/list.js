33eeac33ca4d65d9c57f34bc759c33efe8a93caf
Cleiton carlos <cleitoncarlos.programmer@gmail.com>
2024-06-20 17:20:02 -0300

    exporta o buttonDelete

--- src/scripts/list.js
+++ src/scripts/list.js
@@ -3,7 +3,11 @@ import listing from './listing.js'
 
 const { data } = await supabase.from('users').select('*')
 const content = document.querySelector('.content')
+let buttonDelete
 
 data.map(element => {
   content.innerHTML += listing(element.name)
+  buttonDelete = document.querySelectorAll('.content__buttonDelete')
 })
+
+export default buttonDelete
