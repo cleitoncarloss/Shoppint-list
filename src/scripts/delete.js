379bf430b7614a97a47d798169e98db4863c29b7
Cleiton carlos <cleitoncarlos.programmer@gmail.com>
2024-06-20 17:18:48 -0300

    cria o script para fazer o delete do item da compra

---
+++ src/scripts/delete.js
@@ -0,0 +1,18 @@
+import supabase from './supabase.js'
+import buttonDelete from './list.js'
+
+let text
+buttonDelete.forEach(button => {
+	button.addEventListener('click', async function() {
+		const brother = button.parentElement
+		const father = brother.parentElement
+		text = father.children[0].textContent
+	
+		father.remove()
+
+		const { data } = await supabase
+			.from('users')
+			.delete()
+			.eq('name', text)
+	})
+})
