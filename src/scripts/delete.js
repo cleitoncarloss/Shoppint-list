44d1d5f4e710c7f07a875b9b2d7523cfa1383454
Cleiton carlos <cleitoncarlos.programmer@gmail.com>
2024-06-30 09:45:40 -0300

    declara a variavel em escopo local

--- src/scripts/delete.js
+++ src/scripts/delete.js
@@ -1,13 +1,11 @@
 import supabase from './supabase.js'
 import { buttonDelete } from './list.js'
 
-let text
 buttonDelete.forEach(button => {
 	button.addEventListener('click', async function() {
 		const brother = button.parentElement
 		const father = brother.parentElement
-		text = father.children[0].textContent
-
+		const text = father.children[0].textContent
 		father.remove()
 
 		await supabase
