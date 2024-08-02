5d49530cf9b95836467047a780dd82c2e9fa5d13
Cleiton carlos <cleitoncarlos.programmer@gmail.com>
2024-08-01 21:26:54 -0300

    atualiza o nome da pasta

--- public/scripts/componentEdit.js
+++
@@ -1,8 +0,0 @@
-function componentEdit(value) {
-  return `
-    <input class='edit__input' type='text' value='${value}' /> 
-    <button class='edit__save'>Salvar</button>
-  `
-}
-
-export default componentEdit
